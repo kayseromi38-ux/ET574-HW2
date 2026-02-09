d4796f1806cf2c04a786727e4c628caf91dbdc87
kayseromi38-ux <kayseromi38@gmail.com>
2026-02-08 20:50:22 -0500

    Add Task A, B, and C for HW2
    completed Python tasks and verified output

---
+++ git.py
@@ -0,0 +1,11 @@
+# ET574 - Hw2
+# Name: Kayser Ahmed Omi
+
+# Task A
+print("Task A: Hello from Git!")
+
+# Task B
+print("Task B: Learning Github is step-by-step.")
+
+# task c
+print("Task C: Python + Git = success.")
